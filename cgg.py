61e6cd7ad26c3a4877978e778053b166e9096b79
Gwenna Breton <Gwenna.breton@gu.se>
2022-01-17 12:34:20 +0100

    Create cgg.py

---
+++ cgg.py
@@ -0,0 +1,35 @@
+import passwords
+
+# Token generation
+from requests_oauthlib import OAuth2Session
+
+class OAuth2Client:
+    def __init__(self):
+        self.username = passwords.alissa.username
+        self.password = passwords.alissa.password
+        self.client_id = passwords.alissa.username
+        self.client_secret = passwords.alissa.password
+        self.token_url = passwords.alissa.token_url
+        self.session = OAuth2Session(client=LegacyApplicationClient(client_id=self.client_id))
+
+        self._token = None
+
+    def fetch_token(self):
+
+        if self._token:
+            return self._token
+
+        response = self.session.fetch_token(token_url=self.token_url,
+            username=self.username,
+            password=self.password,
+            client_id=self.client_id,
+            client_secret=self.client_secret)
+
+        if self.session.authorized:
+            self._token = response['token_type'] + " " + response['access_token']
+
+        self.is_valid_toke(self._token)
+        return self._token
+
+    def is_valid_token(self) -> bool:
+        return self.session.authorized
