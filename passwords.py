cbf4843594d98118f1374bbdbf5115619735c9f0
Gwenna Breton <Gwenna.breton@gu.se>
2022-01-19 15:13:54 +0100

    Added base_url

--- passwords.py
+++ passwords.py
@@ -4,3 +4,4 @@ class alissa:
     username = os.environ.get('ALISSA_USERNAME')
     password = os.environ.get('ALISSA_PASSWORD')
     token_url = os.environ.get('ALISSA_TOKEN_URL')
+    base_url = os.environ.get('ALISSA_BASE_URL')
