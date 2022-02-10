0c5596633f2ff9bcd259103fc64a8af67adda601
Gwenna Breton <gwenna.breton@gu.se>
2022-02-10 16:26:57 +0100

    Replaced vcftools by bcftools

--- chunk_vcf.py
+++ chunk_vcf.py
@@ -23,41 +23,28 @@ if original_size == 0:
 
 elif original_size >= 250_000_000:
     print("The VCF file needs to be splitted into smaller VCFs.")
+    
+    #Preliminary step: index the VCF.
+    #TODO possibly: add a clause to check whether the index already exists.
+    command_index = ["bcftools", "index", path_to_original_vcf]
+    subprocess.run(command_index)
+    
     vcf1 = original_folder+"/"+basename+"_chr1-8.vcf.gz"
     vcf2 = original_folder+"/"+basename+"_chr9-hs37d5.vcf.gz"
 
     #Split the VCF.
-    command_compress = ["bgzip", "-c"]
+    command_split_vcf1 = ["bcftools", "view", "--output-type", "z", "--output", vcf1, "-r", "1,2,3,4,5,6,7,8", path_to_original_vcf]
+    command_split_vcf2 = ["bcftools", "view", "--output-type", "z", "--output", vcf2, "-r", "9,10,11,12,13,14,15,16,17,18,19,20,21,22,X,Y,MT,GL000207.1,GL000226.1,GL000229.1,GL000231.1,GL000210.1,GL000239.1,GL000235.1,GL000201.1,GL000247.1,GL000245.1,GL000197.1,GL000203.1,GL000246.1,GL000249.1,GL000196.1,GL000248.1,GL000244.1,GL000238.1,GL000202.1,GL000234.1,GL000232.1,GL000206.1,GL000240.1,GL000236.1,GL000241.1,GL000243.1,GL000242.1,GL000230.1,GL000237.1,GL000233.1,GL000204.1,GL000198.1,GL000208.1,GL000191.1,GL000227.1,GL000228.1,GL000214.1,GL000221.1,GL000209.1,GL000218.1,GL000220.1,GL000213.1,GL000211.1,GL000199.1,GL000217.1,GL000216.1,GL000215.1,GL000205.1,GL000219.1,GL000224.1,GL000223.1,GL000195.1,GL000212.1,GL000222.1,GL000200.1,GL000193.1,GL000194.1,GL000225.1,GL000192.1,NC_007605,hs37d5", path_to_original_vcf]
 
-    command_split_vcf1 = ["vcftools", "--gzvcf", path_to_original_vcf,
-                          "--chr", "1", "--chr", "2", "--chr", "3", "--chr", "4", "--chr", "5", "--chr", "6", "--chr", "7", "--chr", "8", "--recode", "--recode-INFO-all", "--stdout"]
     if os.path.isfile(vcf1):
         print(f'File {vcf1} already exists, moving on.')
     else:
-        with open(vcf1, "w") as f1:
-            split = subprocess.Popen(command_split_vcf1, stdout = subprocess.PIPE)
-            subprocess.run(command_compress, stdin = split.stdout, stdout = f1)
+        subprocess.run(command_split_vcf1)
 
     if os.path.isfile(vcf2):
         print(f'File {vcf2} already exists, moving on.')
     else:
-        command_split_vcf2 = ["vcftools", "--gzvcf", path_to_original_vcf,
-                              "--chr", "9", "--chr", "10", "--chr", "11", "--chr", "12", "--chr", "13", "--chr", "14", "--chr", "15", "--chr", "16",
-                              "--chr", "17", "--chr", "18", "--chr", "19", "--chr", "20", "--chr", "21", "--chr", "22", "--chr", "X", "--chr", "Y", "--chr", "MT",
-                              "--chr", "GL000207.1", "--chr", "GL000226.1", "--chr", "GL000229.1", "--chr", "GL000231.1", "--chr", "GL000210.1", "--chr", "GL000239.1",
-                              "--chr", "GL000235.1", "--chr", "GL000201.1", "--chr", "GL000247.1", "--chr", "GL000245.1", "--chr", "GL000197.1", "--chr", "GL000203.1",
-                              "--chr", "GL000246.1", "--chr", "GL000249.1", "--chr", "GL000196.1", "--chr", "GL000248.1", "--chr", "GL000244.1", "--chr", "GL000238.1",
-                              "--chr", "GL000202.1", "--chr", "GL000234.1", "--chr", "GL000232.1", "--chr", "GL000206.1", "--chr", "GL000240.1", "--chr", "GL000236.1",
-                              "--chr", "GL000241.1", "--chr", "GL000243.1", "--chr", "GL000242.1", "--chr", "GL000230.1", "--chr", "GL000237.1", "--chr", "GL000233.1",
-                              "--chr", "GL000204.1", "--chr", "GL000198.1", "--chr", "GL000208.1", "--chr", "GL000191.1", "--chr", "GL000227.1", "--chr", "GL000228.1",
-                              "--chr", "GL000214.1", "--chr", "GL000221.1", "--chr", "GL000209.1", "--chr", "GL000218.1", "--chr", "GL000220.1", "--chr", "GL000213.1",
-                              "--chr", "GL000211.1", "--chr", "GL000199.1", "--chr", "GL000217.1", "--chr", "GL000216.1", "--chr", "GL000215.1", "--chr", "GL000205.1",
-                              "--chr", "GL000219.1", "--chr", "GL000224.1", "--chr", "GL000223.1", "--chr", "GL000195.1", "--chr", "GL000212.1", "--chr", "GL000222.1",
-                              "--chr", "GL000200.1", "--chr", "GL000193.1", "--chr", "GL000194.1", "--chr", "GL000225.1", "--chr", "GL000192.1", "--chr", "NC_007605", "--chr", "hs37d5",
-                              "--recode", "--recode-INFO-all", "--stdout"]
-        with open(vcf2, "w") as f2:
-            split = subprocess.Popen(command_split_vcf2, stdout = subprocess.PIPE)
-            subprocess.run(command_compress, stdin = split.stdout, stdout = f2)
+        subprocess.run(command_split_vcf2)
 
     #Check that the new sizes are less than 250000000.
     #At the moment, manual intervention will be needed if the new files are still larger than the limit (e.g. to decide how to split the VCFs).
