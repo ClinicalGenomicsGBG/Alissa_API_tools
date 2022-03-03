fe7d8e899e735dec1fad188c2222a8887a9bba13
Gwenna Breton <gwenna.breton@gu.se>
2022-03-03 14:13:19 +0100

    Moved logic to check file format from main() to bgzip().

--- chunk_vcf.py
+++ chunk_vcf.py
@@ -23,11 +23,17 @@ def vcf_larger_than(vcf,size):
     return bool(vcf_size > size)
 
 def bgzip(vcf,outfolder):
-    """Bgzip a VCF."""
-    basename = os.path.basename(vcf)
-    bgzipped_vcf = os.path.join(outfolder, basename + ".gz")
-    command_bgzip = ["bcftools", "view", "--output-type", "z", "--output", bgzipped_vcf, vcf]
-    subprocess.run(command_bgzip)
+    """Check whether input is .vcf or .vcf.gz. If it is .vcf, bgzip it."""
+    if vcf.endswith('vcf.gz'):
+        bgzipped_vcf = vcf
+    elif vcf.endswith('vcf'):
+        print("The VCF will be bgzipped.")
+        basename = os.path.basename(vcf)
+        bgzipped_vcf = os.path.join(outfolder, basename + ".gz")
+        command_bgzip = ["bcftools", "view", "--output-type", "z", "--output", bgzipped_vcf, vcf]
+        subprocess.run(command_bgzip)
+    else:
+        raise Exception('The input file should be a VCF (.vcf or .vcf.gz).')
     return bgzipped_vcf
 
 def index(vcf):
@@ -45,17 +51,14 @@ def split(vcf,outfolder):
     vcf2 = os.path.join(outfolder,basename+"_chr9-hs37d5.vcf.gz")
     command_split_vcf1 = ["bcftools", "view", "--output-type", "z", "--output", vcf1, "-r", "1,2,3,4,5,6,7,8", vcf]
     command_split_vcf2 = ["bcftools", "view", "--output-type", "z", "--output", vcf2, "-r", "9,10,11,12,13,14,15,16,17,18,19,20,21,22,X,Y,MT,GL000207.1,GL000226.1,GL000229.1,GL000231.1,GL000210.1,GL000239.1,GL000235.1,GL000201.1,GL000247.1,GL000245.1,GL000197.1,GL000203.1,GL000246.1,GL000249.1,GL000196.1,GL000248.1,GL000244.1,GL000238.1,GL000202.1,GL000234.1,GL000232.1,GL000206.1,GL000240.1,GL000236.1,GL000241.1,GL000243.1,GL000242.1,GL000230.1,GL000237.1,GL000233.1,GL000204.1,GL000198.1,GL000208.1,GL000191.1,GL000227.1,GL000228.1,GL000214.1,GL000221.1,GL000209.1,GL000218.1,GL000220.1,GL000213.1,GL000211.1,GL000199.1,GL000217.1,GL000216.1,GL000215.1,GL000205.1,GL000219.1,GL000224.1,GL000223.1,GL000195.1,GL000212.1,GL000222.1,GL000200.1,GL000193.1,GL000194.1,GL000225.1,GL000192.1,NC_007605,hs37d5", vcf]
-
     if os.path.isfile(vcf1):
         print(f'File {vcf1} already exists, moving on.')
     else:
         subprocess.run(command_split_vcf1)
-
     if os.path.isfile(vcf2):
         print(f'File {vcf2} already exists, moving on.')
     else:
         subprocess.run(command_split_vcf2)
-
     return [vcf1, vcf2]
 
 
@@ -66,18 +69,8 @@ def main():
         raise Exception(f'Please check this file: {path_to_original_vcf}, the size is 0.')
 
     else:
-        #Identify whether the file is gzipped. If no, it needs to be compressed.
-        # Should the logic to check whether the file is already compressed be in bgzip() ?
-        if path_to_original_vcf.endswith('vcf.gz'):
-            vcf_to_index = path_to_original_vcf
-
-        elif path_to_original_vcf.endswith('vcf'):
-            print("The VCF will be bgzipped.")
-            vcf_to_index = bgzip(path_to_original_vcf,output_folder)
+        vcf_to_index = bgzip(path_to_original_vcf,output_folder)
  
-        else:
-            raise Exception('The input file should be a VCF (.vcf or .vcf.gz).')
-        
         if vcf_larger_than(vcf_to_index,240_000_000):
             print("The file is larger than 240 MiB and will be splitted into two VCFs.")
                   
