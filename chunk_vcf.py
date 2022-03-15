3a8243e88bf53ef7145f7f23409d387697b7863b
Gwenna Breton <gwenna.breton@gu.se>
2022-03-15 16:41:57 +0100

    Debug new functions
    - Fixed how new item is appended to list
    - Cleaned comments
    - Added commented code for split in 3 and 4

--- chunk_vcf.py
+++ chunk_vcf.py
@@ -38,7 +38,6 @@ def index(vcf):
 def split_vcf(vcf, root, suffix, regions):
 #    output = os.path.join(root, suffix + '.vcf.gz')
     output = root + suffix + '.vcf.gz' #TODO replace by something nicer. os.path.join does not give the expected result (removes the file name from root).
-    print(output)
     command_split = ["bcftools", "view", "--output-type", "z", "--output", output, "-r", regions, vcf]
     if os.path.exists(output):
         print(f'File {output} already exists, moving on.')
@@ -64,7 +63,6 @@ def prepare_chunk(vcf, outfolder, size):
         
         basename = os.path.basename(vcf).replace('.vcf.gz', '')
         root = os.path.join(outfolder, basename)
-        print(root)
 
         #TODO move the comment about regions somewhere else.
         #TODO decide where the "split_in_2/3/4" lists should be. Here in the function, or in a config file?
@@ -74,19 +72,25 @@ def prepare_chunk(vcf, outfolder, size):
         split_in_4 = [["_chr1-4","1,2,3,4"], ["_chr5-8","5,6,7,8"], ["_chr9-14","9,10,11,12,13,14"], ["_chr15-hs37d5","15,16,17,18,19,20,21,22,X,Y,MT,GL000207.1,GL000226.1,GL000229.1,GL000231.1,GL000210.1,GL000239.1,GL000235.1,GL000201.1,GL000247.1,GL000245.1,GL000197.1,GL000203.1,GL000246.1,GL000249.1,GL000196.1,GL000248.1,GL000244.1,GL000238.1,GL000202.1,GL000234.1,GL000232.1,GL000206.1,GL000240.1,GL000236.1,GL000241.1,GL000243.1,GL000242.1,GL000230.1,GL000237.1,GL000233.1,GL000204.1,GL000198.1,GL000208.1,GL000191.1,GL000227.1,GL000228.1,GL000214.1,GL000221.1,GL000209.1,GL000218.1,GL000220.1,GL000213.1,GL000211.1,GL000199.1,GL000217.1,GL000216.1,GL000215.1,GL000205.1,GL000219.1,GL000224.1,GL000223.1,GL000195.1,GL000212.1,GL000222.1,GL000200.1,GL000193.1,GL000194.1,GL000225.1,GL000192.1,NC_007605,hs37d5"]]
 
         #Split in two.
-        #TODO add a condition or logic for splitting in two (and not three or four).
+        #TODO add a condition. Option 1: based on the size of the VCF, decide to split in 2, 3 or 4. Option 2: split in 2, check size, split in 3 if needed, check size, split in 4 if needed.
         splitted = []
         for partition in split_in_2:
-            print(partition[0])
             chunk = split_vcf(vcf, root, partition[0], partition[1])
-            splitted = splitted.append(chunk)
+            splitted.append(chunk)
       
-        #Check the size of the two chunks. If one is larger than the given size: split in three.
-        #TODO if the splitting logic above works: do the same for three chunks.
+        #Split in three.
+        #TODO add a condition.
+#        for partition in split_in_3:
+#            chunk = split_vcf(vcf, root, partition[0], partition[1])
+#            splitted.append(chunk)
 
-        #Check the size of the three chunks. If one is larger than the given size: split in four.
+        #Split in four.
+        #TODO add a condition.
+#        for partition in split_in_4:
+#            chunk = split_vcf(vcf, root, partition[0], partition[1])
+#            splitted.append(chunk)
 
-#            if any([vcf_larger_than(newvcf, size) for newvcf in [vcf1A, vcf1B, vcf2A, vcf2B]]):
+#            if any([vcf_larger_than(newvcf, size) for newvcf in splitted ]):
 #                raise Exception(f'One of the files is still larger than {size} bytes. Please investigate.')
                 
     return splitted
