3409def421dc17338d2183bc200a225ccb28c06a
Gwenna Breton <gwenna.breton@gu.se>
2022-03-18 15:25:59 +0100

    Modified logic and documentation

--- chunk_vcf.py
+++ chunk_vcf.py
@@ -34,14 +34,14 @@ def index(vcf):
     else:
         subprocess.run(command_index)
 
-#TODO this is a draft - once I have written it out more or less, see which arguments are needed etc. Would a class be better? "basename" is defined several times I think. Etc.
-def split_vcf(vcf, root, suffix, regions):
-#    output = os.path.join(root, suffix + '.vcf.gz')
+def split_vcf(vcf, outfolder, suffix, regions):
+    """Return a VCF file that contains a subset of regions from the input VCF. Arguments outfolder and suffix are used to name the output file."""
+    basename = os.path.basename(vcf).replace('.vcf.gz', '')
+    root = os.path.join(outfolder, basename)
     output = root + suffix + '.vcf.gz' #TODO replace by something nicer. os.path.join does not give the expected result (removes the file name from root).
     command_split = ["bcftools", "view", "--output-type", "z", "--output", output, "-r", regions, vcf]
     if os.path.exists(output):
         print(f'File {output} already exists, moving on.')
-        #What should happen then? "pass"?
     else:
         subprocess.run(command_split)
     return output
@@ -61,9 +61,6 @@ def prepare_chunk(vcf, outfolder, size):
     else:
         print(f'The file is larger than {size} bytes and will be splitted into two VCFs.')
         
-        basename = os.path.basename(vcf).replace('.vcf.gz', '')
-        root = os.path.join(outfolder, basename)
-
         #TODO decide where the "split_in_2/3/4" lists should be. Here in the function, or in a config file?
         split_in_2 = [["_chr1-8","1,2,3,4,5,6,7,8"], ["_chr9-hs37d5","9,10,11,12,13,14,15,16,17,18,19,20,21,22,X,Y,MT,GL000207.1,GL000226.1,GL000229.1,GL000231.1,GL000210.1,GL000239.1,GL000235.1,GL000201.1,GL000247.1,GL000245.1,GL000197.1,GL000203.1,GL000246.1,GL000249.1,GL000196.1,GL000248.1,GL000244.1,GL000238.1,GL000202.1,GL000234.1,GL000232.1,GL000206.1,GL000240.1,GL000236.1,GL000241.1,GL000243.1,GL000242.1,GL000230.1,GL000237.1,GL000233.1,GL000204.1,GL000198.1,GL000208.1,GL000191.1,GL000227.1,GL000228.1,GL000214.1,GL000221.1,GL000209.1,GL000218.1,GL000220.1,GL000213.1,GL000211.1,GL000199.1,GL000217.1,GL000216.1,GL000215.1,GL000205.1,GL000219.1,GL000224.1,GL000223.1,GL000195.1,GL000212.1,GL000222.1,GL000200.1,GL000193.1,GL000194.1,GL000225.1,GL000192.1,NC_007605,hs37d5"]]
         split_in_3 = [["_chr1-5","1,2,3,4,5"], ["_chr6-12","6,7,8,9,10,11,12"], ["_chr13-hs37d5","13,14,15,16,17,18,19,20,21,22,X,Y,MT,GL000207.1,GL000226.1,GL000229.1,GL000231.1,GL000210.1,GL000239.1,GL000235.1,GL000201.1,GL000247.1,GL000245.1,GL000197.1,GL000203.1,GL000246.1,GL000249.1,GL000196.1,GL000248.1,GL000244.1,GL000238.1,GL000202.1,GL000234.1,GL000232.1,GL000206.1,GL000240.1,GL000236.1,GL000241.1,GL000243.1,GL000242.1,GL000230.1,GL000237.1,GL000233.1,GL000204.1,GL000198.1,GL000208.1,GL000191.1,GL000227.1,GL000228.1,GL000214.1,GL000221.1,GL000209.1,GL000218.1,GL000220.1,GL000213.1,GL000211.1,GL000199.1,GL000217.1,GL000216.1,GL000215.1,GL000205.1,GL000219.1,GL000224.1,GL000223.1,GL000195.1,GL000212.1,GL000222.1,GL000200.1,GL000193.1,GL000194.1,GL000225.1,GL000192.1,NC_007605,hs37d5"]]
@@ -72,7 +69,7 @@ def prepare_chunk(vcf, outfolder, size):
         #Split in two.
         splitted = []
         for partition in split_in_2:
-            chunk = split_vcf(vcf, root, partition[0], partition[1])
+            chunk = split_vcf(vcf, outfolder, partition[0], partition[1])
             splitted.append(chunk)
 
         if any([vcf_larger_than(newvcf, size) for newvcf in splitted ]):       
@@ -80,7 +77,7 @@ def prepare_chunk(vcf, outfolder, size):
             #Split in three.
             splitted = []
             for partition in split_in_3:
-                chunk = split_vcf(vcf, root, partition[0], partition[1])
+                chunk = split_vcf(vcf, outfolder, partition[0], partition[1])
                 splitted.append(chunk)
 
             if any([vcf_larger_than(newvcf, size) for newvcf in splitted ]):
@@ -88,7 +85,7 @@ def prepare_chunk(vcf, outfolder, size):
                 #Split in four.
                 splitted = []
                 for partition in split_in_4:
-                    chunk = split_vcf(vcf, root, partition[0], partition[1])
+                    chunk = split_vcf(vcf, outfolder, partition[0], partition[1])
                     splitted.append(chunk)
 
                 if any([vcf_larger_than(newvcf, size) for newvcf in splitted ]):
@@ -97,6 +94,7 @@ def prepare_chunk(vcf, outfolder, size):
     return splitted
         
 def prepare_and_split_vcf(vcf, outfolder, size):
+    """Perform preliminary checks on input and return one to four VCF.GZ smaller than the given size."""
     #Check input format.
     if not (vcf.endswith('vcf.gz') or vcf.endswith('vcf')):
         raise Exception('The input file should be a VCF (.vcf or .vcf.gz).') 
