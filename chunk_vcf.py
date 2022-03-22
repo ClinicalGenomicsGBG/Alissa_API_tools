feb90cfe29becda13fa3a273a4af813cb29216a4
Gwenna Breton <gwenna.breton@gu.se>
2022-03-22 10:35:53 +0100

    Refactored prepare_chunk():
    - Replaced nested commands by a while loop
    - Cleaned code of old things

--- chunk_vcf.py
+++ chunk_vcf.py
@@ -5,13 +5,11 @@
 import sys
 import os
 import subprocess
+import lists_of_contigs
 
-#args = sys.argv
-#path_to_original_vcf = str(args[1]).strip()
-#output_folder = str(args[2]).strip()
 path_to_original_vcf = '/home/xbregw/Alissa_upload/VCFs/NA24143_191108_AHVWHGDSXX_SNV_CNV_germline.vcf.gz'
 output_folder = '/home/xbregw/Alissa_upload/VCFs/chunks'
-max_size = 100_000_000
+max_size = 240_000_000
 
 def vcf_larger_than(vcf, size):
     """Return True if the size of the VCF is larger than a given size."""
@@ -57,43 +55,24 @@ def prepare_chunk(vcf, outfolder, size):
     Return an exception if one of the four chunks is larger than the given size. 
     """
     
-    if not vcf_larger_than(vcf, size):
-        print(f'The file is smaller than {size} bytes. It will not be splitted.')
-        splitted = [vcf]
+    splitted = [vcf]
+    toobig = any([vcf_larger_than(newvcf, size) for newvcf in splitted ]) #Initialize boolean that returns True if at least one of the files is too large.
+    i = 0 #Initialize count variable for while loop
 
-    else:
-        print(f'The file is larger than {size} bytes and will be splitted into two VCFs.')
-        
-        #TODO decide where the "split_in_2/3/4" lists should be. Here in the function, or in a config file?
-        split_in_2 = [[".chr1-8","1,2,3,4,5,6,7,8"], [".chr9-hs37d5","9,10,11,12,13,14,15,16,17,18,19,20,21,22,X,Y,MT,GL000207.1,GL000226.1,GL000229.1,GL000231.1,GL000210.1,GL000239.1,GL000235.1,GL000201.1,GL000247.1,GL000245.1,GL000197.1,GL000203.1,GL000246.1,GL000249.1,GL000196.1,GL000248.1,GL000244.1,GL000238.1,GL000202.1,GL000234.1,GL000232.1,GL000206.1,GL000240.1,GL000236.1,GL000241.1,GL000243.1,GL000242.1,GL000230.1,GL000237.1,GL000233.1,GL000204.1,GL000198.1,GL000208.1,GL000191.1,GL000227.1,GL000228.1,GL000214.1,GL000221.1,GL000209.1,GL000218.1,GL000220.1,GL000213.1,GL000211.1,GL000199.1,GL000217.1,GL000216.1,GL000215.1,GL000205.1,GL000219.1,GL000224.1,GL000223.1,GL000195.1,GL000212.1,GL000222.1,GL000200.1,GL000193.1,GL000194.1,GL000225.1,GL000192.1,NC_007605,hs37d5"]]
-        split_in_3 = [[".chr1-5","1,2,3,4,5"], [".chr6-12","6,7,8,9,10,11,12"], [".chr13-hs37d5","13,14,15,16,17,18,19,20,21,22,X,Y,MT,GL000207.1,GL000226.1,GL000229.1,GL000231.1,GL000210.1,GL000239.1,GL000235.1,GL000201.1,GL000247.1,GL000245.1,GL000197.1,GL000203.1,GL000246.1,GL000249.1,GL000196.1,GL000248.1,GL000244.1,GL000238.1,GL000202.1,GL000234.1,GL000232.1,GL000206.1,GL000240.1,GL000236.1,GL000241.1,GL000243.1,GL000242.1,GL000230.1,GL000237.1,GL000233.1,GL000204.1,GL000198.1,GL000208.1,GL000191.1,GL000227.1,GL000228.1,GL000214.1,GL000221.1,GL000209.1,GL000218.1,GL000220.1,GL000213.1,GL000211.1,GL000199.1,GL000217.1,GL000216.1,GL000215.1,GL000205.1,GL000219.1,GL000224.1,GL000223.1,GL000195.1,GL000212.1,GL000222.1,GL000200.1,GL000193.1,GL000194.1,GL000225.1,GL000192.1,NC_007605,hs37d5"]]
-        split_in_4 = [[".chr1-4","1,2,3,4"], [".chr5-8","5,6,7,8"], [".chr9-14","9,10,11,12,13,14"], [".chr15-hs37d5","15,16,17,18,19,20,21,22,X,Y,MT,GL000207.1,GL000226.1,GL000229.1,GL000231.1,GL000210.1,GL000239.1,GL000235.1,GL000201.1,GL000247.1,GL000245.1,GL000197.1,GL000203.1,GL000246.1,GL000249.1,GL000196.1,GL000248.1,GL000244.1,GL000238.1,GL000202.1,GL000234.1,GL000232.1,GL000206.1,GL000240.1,GL000236.1,GL000241.1,GL000243.1,GL000242.1,GL000230.1,GL000237.1,GL000233.1,GL000204.1,GL000198.1,GL000208.1,GL000191.1,GL000227.1,GL000228.1,GL000214.1,GL000221.1,GL000209.1,GL000218.1,GL000220.1,GL000213.1,GL000211.1,GL000199.1,GL000217.1,GL000216.1,GL000215.1,GL000205.1,GL000219.1,GL000224.1,GL000223.1,GL000195.1,GL000212.1,GL000222.1,GL000200.1,GL000193.1,GL000194.1,GL000225.1,GL000192.1,NC_007605,hs37d5"]]
-
-        #Split in two.
+    while i < len(lists_of_contigs.contig_lists) and toobig :
         splitted = []
-        for partition in split_in_2:
+        for partition in lists_of_contigs.contig_lists[i] :
             chunk = split_vcf(vcf, outfolder, partition[0], partition[1])
-            splitted.append(chunk)
+            splitted.append(chunk)        
+        i = i + 1
+        toobig = any([vcf_larger_than(newvcf, size) for newvcf in splitted ])
+        print(toobig)
+        j = i + 1
+        print(f'The VCF has been split into {j} chunks.')
 
-        if any([vcf_larger_than(newvcf, size) for newvcf in splitted ]):       
-            print(f'At least one of the files is larger than {size} bytes. The input will be splitted into three VCFs.')
-            #Split in three.
-            splitted = []
-            for partition in split_in_3:
-                chunk = split_vcf(vcf, outfolder, partition[0], partition[1])
-                splitted.append(chunk)
-
-            if any([vcf_larger_than(newvcf, size) for newvcf in splitted ]):
-                print(f'At least one of the files is larger than {size} bytes. The input will be splitted into four VCFs.')
-                #Split in four.
-                splitted = []
-                for partition in split_in_4:
-                    chunk = split_vcf(vcf, outfolder, partition[0], partition[1])
-                    splitted.append(chunk)
-
-                if any([vcf_larger_than(newvcf, size) for newvcf in splitted ]):
-                    raise Exception(f'One of the files is still larger than {size} bytes. Please investigate.')
-                
+    if toobig:
+        raise Exception(f'We ran out of chunks and one of the files is still larger than {size} bytes. Please investigate.')
+       
     return splitted
         
 def prepare_and_split_vcf(vcf, outfolder, size):
