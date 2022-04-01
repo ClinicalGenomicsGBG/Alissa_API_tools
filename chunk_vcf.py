a0e39d2aeb8b4d84ce08f8db38622a3e8e80e780
Gwenna Breton <gwenna.breton@gu.se>
2022-04-01 17:29:21 +0200

    Started adding logging to chunk_vcf.py:
    - Setup logger in function prepare_and_split_vcf
    - Move the information sentences from the functions into prepare_and_list_vcf
    - Test and debug

--- chunk_vcf.py
+++ chunk_vcf.py
@@ -6,24 +6,30 @@ import subprocess
 import lists_of_contigs
 import click
 
+from tools.helpers import setup_logger
+
 def bgzip(vcf, outfolder):
     """Bgzip VCF file."""
     basename = os.path.basename(vcf)
     bgzipped_vcf = os.path.join(outfolder, basename + ".gz")
     command_bgzip = ["bcftools", "view", "--output-type", "z", "--output", bgzipped_vcf, vcf]
     if os.path.isfile(bgzipped_vcf):
-        print('File exists already, skipping compression.')
+        bgzipped_exist = True
     else:
         subprocess.run(command_bgzip)
-    return bgzipped_vcf
+
+    return bgzipped_vcf, bgzipped_exist
 
 def index(vcf):
     """Index a bgzipped VCF if the index cannot be found."""
     command_index = ["bcftools", "index", vcf]
     if os.path.isfile(os.path.join(vcf + ".csi")):
-        print('Index exists already, skipping indexing.')
+        index_exist = True
     else:
         subprocess.run(command_index)
+        index_exist = False
+
+    return index_exist
 
 def split_vcf(vcf, outfolder, suffix, regions):
     """Return a VCF file that contains a subset of regions from the input VCF. Arguments outfolder and suffix are used to name the output file."""
@@ -46,10 +52,13 @@ def prepare_chunk(vcf, outfolder, size):
     """
     
     if os.path.getsize(vcf) < size:
-        print('The VCF did not need to be split.')
-        return [vcf]
+#        print('The VCF did not need to be split.')
+        split_status = False
+        n_chunks = 0
+        return [vcf], split_status, n_chunks
 
     else:
+        split_status = True
         for chunk_attempt in lists_of_contigs.contig_lists:
             chunks = []
             for chunk in chunk_attempt:
@@ -57,36 +66,60 @@ def prepare_chunk(vcf, outfolder, size):
                 chunks.append(chunk_path)
 
             if not any([os.path.getsize(chunk) > size for chunk in chunks]):
-                l = len(chunks)
-                print(f'The VCF has been split into {l} chunks.')
-                return chunks
+                n_chunks = len(chunks)
+#                print(f'The VCF has been split into {l} chunks.')
+                return chunks, split_status, n_chunks
         else:
+            #TODO how should I deal with that for the logger? I do not fully understand the "else" logic here.
             raise Exception(f'We ran out of chunks and one of the files is still larger than {size} bytes. Please investigate.')
         
 def prepare_and_split_vcf(vcf, outfolder, size):
     """Perform preliminary checks on input and return one to four VCF.GZ smaller than the given size."""
-    #Check input format.
-    if not (vcf.endswith('vcf.gz') or vcf.endswith('vcf')):
-        raise Exception('The input file should be a VCF (.vcf or .vcf.gz).') 
+    ## Set up the logfile and start logging.
+    # TODO is it weird that this is here and not in main() ? The point of having all the steps here was that one could import the function prepare_and_split_vcf (nicer than having to import main).
+    logger = setup_logger('prepare_and_split_vcf')
 
-    #Check that input is not empty.
+    logger.info(f'Starting preparation of file {vcf}')
+
+    ## Check input format.
+    logger.info(f'Checking format of {vcf}')
+    if not (vcf.endswith('vcf.gz') or vcf.endswith('vcf')):
+        logger.error('The input file should be a VCF (.vcf or .vcf.gz).') 
+        raise Exception
+
+    ## Check that input is not empty.
     if os.path.getsize(vcf) == 0:
-        raise Exception(f'Please check this file: {vcf}, the size is 0.')
+        logger.error(f'Please check this file: {vcf}, the size is 0.')
+        raise Exception
 
     else:
         if vcf.endswith('vcf'):
-            print("The VCF will be bgzipped.")
-            unindexed_vcf = bgzip(vcf, outfolder)
+            logger.info("The VCF will be bgzipped.")
+            unindexed_vcf, bgzipped_exists = bgzip(vcf, outfolder)
+            if bgzipped_exists == True:
+                logger.info(f'A bgzipped version of the file exists already, using that instead.')
+            else:
+                logger.info('The file has been bgzipped.')
 
         else:
             unindexed_vcf = vcf
  
-        #Index the VCF.
-        index(unindexed_vcf)
-            
-        #Split the VCF.
-        new_vcfs = prepare_chunk(unindexed_vcf, outfolder, size)
-            
+        ## Index the VCF.
+        logger.info('Starting indexing of the input file.')
+        index_exists = index(unindexed_vcf)
+        if index_exists == True:
+            logger.info(f'There is already an index for {unindexed_vcf}, skipping indexing.')
+        else:
+            logger.info(f'The file {unindexed_vcf} has been indexed.')
+    
+        ## Split the VCF.
+        logger.info(f'Checking the size of the file and starting chunking if relevant.')
+        new_vcfs, status, n_chunks = prepare_chunk(unindexed_vcf, outfolder, size)
+        if status == False:
+            logger.info(f'The input file is smaller than {size} and does not need to be split.')
+        else:
+            logger.info(f'The input file has been split in {n_chunks} chunks.')
+    
         return new_vcfs
 
 @click.command()
